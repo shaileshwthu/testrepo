c614028c7abca9e01f0ce76c25985f18bbef59bd
shaileswthu@gmail.com <user@user-VAIO>
2014-09-10 21:40:46 -0500

    this is a demo

---
+++ test/src/test/Display.java
@@ -0,0 +1,20 @@
+package test;
+
+public class Display {
+
+	private String name;
+
+	public Display(String name) {
+		this.name = name;
+	}
+
+	public void displayName() {
+		System.out.println(name);
+	}
+
+	public static void main(String[] args) {
+		Display display = new Display("My name is shailesh");
+		display.displayName();
+	}
+
+}
